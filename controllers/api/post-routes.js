2f56ee5e297f1cb95f131562fa7f792238748868
yaneyalvarado <alvyaney@gmail.com>
2023-05-29 14:20:56 -0600

    added post-routes.js file and added code

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,100 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../../models');
+const sequelize = require('../../config/connection');
+const withAuth = require('../../utils/auth');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: ['id', 'title', 'post_content', 'created_at'],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Comment, 
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        resizeTo.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'created_at',
+            'post_content'
+        ],
+        include: [
+            {
+                model: User, 
+                attributes: ['username', 'github']
+            },
+            model: Comment, 
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User, 
+                attributes: ['username', 'github']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found.' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/', withAuth, (req,res) => {
+    Post.create({
+        title: req.body.title,
+        post_content: req.body.post_content,
+        user_id: req.session.user_id
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.put('/:id', withAuth, (req, res) => {
+    Post.update({
+        title: req.body.title,
+        post_content: req.body.post_content
+    },
+    {
+        where: {
+            id: req.params.id
+        }
+    })   
+})
+.then(dbPostData => {
+    if (!dbPostData) {
+        res.status(404).json({ message: 'No post found.' });
+        return;
+    }
+    res.json(dbPostData);
+})
+
---
+++ server.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
