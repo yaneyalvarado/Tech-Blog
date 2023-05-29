517845fb899d9216690b8edc91be30eed4a1781e
yaneyalvarado <alvyaney@gmail.com>
2023-05-29 14:48:53 -0600

    created handlebars initialization and connected routes in server.js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -97,4 +97,29 @@ router.put('/:id', withAuth, (req, res) => {
     }
     res.json(dbPostData);
 })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+  });
+
+  router.delete('/:id', withAuth, (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+  });
+
+    module.exports = router;
 
