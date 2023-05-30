ff9e82ea02b0cd6b4dc4c43a8da7938bf65736ce
yaneyalvarado <alvyaney@gmail.com>
2023-05-30 15:02:21 -0600

    updated line 6 in controllers/api/index.js due to error

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,7 +3,7 @@ const userRoutes = require('./user-routes');
 const commentRoutes = require('./comment-routes');
 const postRoutes = require('./comment-routes');
 
-router.use('./users', userRoutes);
+router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
 
