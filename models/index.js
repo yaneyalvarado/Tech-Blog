fa8f0103209fc077aa6a985e396cd8c578e87430
yaneyalvarado <alvyaney@gmail.com>
2023-05-30 22:51:06 -0600

    generated associates in index.js and created views, layout, and partials folders with handlebars files

---
+++ models/index.js
@@ -0,0 +1,30 @@
+const User = require('./user');
+const Post = require('./post');
+const Comment = require('./comments');
+
+// generating associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
