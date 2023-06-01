2c801f74d6106f7069a1c8b7cb0607665070730b
yaneyalvarado <alvyaney@gmail.com>
2023-05-31 18:30:10 -0600

    connect to jawsdb

--- README.md
+++ README.md
@@ -39,7 +39,7 @@ A CMS-style blog where developers can publish their blog posts and comment on ot
 - WHEN I click on the logout option in the navigation
 - THEN I am signed out of the site
 - WHEN I am idle on the site for more than a set time
-- THEN I am able to view posts and comments but I am prompted to log in again before I can add, update, or delete posts`
+- THEN I am able to view posts and comments but I am prompted to log in again before I can add, update, or delete posts
 
 ## GitHub
 
--- config/connection.js
+++ config/connection.js
@@ -5,13 +5,18 @@ require('dotnev').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-    sequelize = new sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    sequelize = new sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
-    });
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    },
+  );
 }
 
-module.exports = sequelize;
+  module.exports = sequelize;
